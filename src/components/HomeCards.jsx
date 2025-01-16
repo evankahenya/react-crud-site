86f5e66728003a6adbc3eebf45c6e11b2c20b63b
Evans <Evans@SYBKE-EVANSON>
2025-01-16 19:25:32 +0300

    problem with home cards

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 
 const HomeCards = () => {
   return (
-    
+    //Home cards
     <section className="py-4">
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
